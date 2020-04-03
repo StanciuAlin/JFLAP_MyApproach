91e10641b93192ac9f0d066d16b0de953775c5fe
Stanciu_Alin <stanciualin26@yahoo.com>
2020-04-03 12:22:22 +0300

    other commit

---
+++ JFLAP_MyImplementation/FiniteAutomatonForm.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JFLAP_MyCopy
+{
+    /// <summary>
+    /// Interaction logic for FiniteAutomatonForm.xaml
+    /// </summary>
+    public partial class FiniteAutomatonForm : Window
+    {
+        public FiniteAutomatonForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ JFLAP_MyImplementation/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JFLAP_MyCopy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_FiniteAutomatonMainForm_Click(object sender, RoutedEventArgs e)
+        {
+            FiniteAutomatonForm finiteAutomatonForm = new FiniteAutomatonForm();
+            this.Visibility = Visibility.Hidden;
+            finiteAutomatonForm.Show();
+        }
+    }
+}
